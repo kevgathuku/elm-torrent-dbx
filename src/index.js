675e339a40b391968d71e1a1da8b4cb02fa6efba
Kevin Ndung'u <kevgathuku@gmail.com>
2017-05-04 16:59:20 +0300

    Add the port to the ws_url only if it exists
    In production, we can ignore the value of the PORT since the ws instance
    can be accessed just from its URL

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ const Elm = require('./Main.elm');
 var node = document.querySelector('#app');
 
 const app = Elm.Main.embed(node, {
-  ws_url:  window.location.hostname + ":" + process.env.PORT
+  ws_url:  window.location.hostname + (process.env.PORT ? ":" + process.env.PORT : "")
 });
