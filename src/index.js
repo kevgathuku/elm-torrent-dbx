f98e31bf38b34950ba9262ff2c770f0017363264
Kevin Ndung'u <kevgathuku@gmail.com>
2017-05-07 23:51:44 +0300

    Simpler way to set websockets url depending on protocol

--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ require('./index.css');
 const Elm = require('./Main.elm');
 var node = document.querySelector('#app');
 
-let prefix;
-if (window.location.protocol === 'http:') {
-  prefix = "ws://"
-} else if (window.location.protocol === 'https:') {
-  prefix = "wss://"
-}
+const prefix = location.protocol === 'https:' ? 'wss://' : 'ws://';
 
 const app = Elm.Main.embed(node, {
   ws_url:  prefix + window.location.hostname + (process.env.PORT ? ":" + process.env.PORT : "")
